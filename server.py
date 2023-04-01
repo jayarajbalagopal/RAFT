edce3864bb896a69610c13959fc30bb875c12f91
Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>
2023-04-01 12:00:00 +0530

    Added zmq communication channel
    Signed-off-by: Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 import logging
+import argparse
+import sys
 from utils import parse_config
+from raft_node import RaftNode
+from raft_logger import logger
 
 
 config = parse_config()
-if config['log_level'] == 'ERROR':
-    log_level = logging.ERROR
-elif config['log_level'] == 'DEBUG':
-    log_level = logging.DEBUG
-else:
-    log_level = loggin.INFO
 
-logging.basicConfig(filename=config['log_file'], encoding='utf-8', level=log_level)
+parser = argparse.ArgumentParser(description='Argument parser for raft server')
+parser.add_argument('--node_id', type=int, help='node_id of the server')
+
+args = parser.parse_args()
+node_id = args.node_id
+
+if node_id is None or node_id < 0 or node_id >= len(config['servers']):
+	logger.error("Invalid node_id")
+	sys.exit(0)
+
+node = RaftNode(node_id, config['servers'])
+node.start()
