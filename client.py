bddf5a0cedb4ae5706d30e6392d3f21315dcbf13
imainakd <mainak.dhara@students.iiit.ac.in>
2023-04-15 13:47:33 +0530

    client implemented

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import requests
 url = 'http://localhost:2000/submit'
 
 while True:
-    key = input('Enter key (or "exit" to quit): ')
-    if key == 'exit':
-        break
-    value = input('Enter value: ')
-    data = {key: value}
+    # key = input('Enter key (or "exit" to quit): ')
+    # if key == 'exit':
+    #     break
+    command = input('Enter command: ')
+    data = {'data': command}
     response = requests.post(url, data=data)
     print(response.text)
